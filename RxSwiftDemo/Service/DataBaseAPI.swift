79e734805c13c97cbd856a2e481943fef173ee82
bitware <bitware@192.168.2.27>
2019-01-09 17:53:30 +0900

    not completed

--- RxSwiftDemo/Service/DataBaseAPI.swift
+++ RxSwiftDemo/Service/DataBaseAPI.swift
@@ -67,4 +67,70 @@ class DataBaseAPI {
             )
         """)
     }
+    
+    func add(repository : Repository) -> Void {
+        db.open()
+        defer {
+            db.close()
+        }
+        
+        var maxID = 0
+        
+        let res: FMResultSet = (try? db.executeQuery("SELECT * FROM repository ", values: nil)) ?? FMResultSet()
+        //获取数据库中最大的ID
+        while res.next() {
+            if maxID < res.int(forColumn: "repository_id") {
+                maxID = Int(res.int(forColumn: "repository_id") )
+            }
+        }
+        maxID = maxID + 1
+        
+        try? db.executeUpdate("INSERT INTO repository(repository_id,name,full_name,html_url,description, comment)VALUES(?,?,?,?,?,?)", values: [maxID, repository.name, repository.fullName, repository.htmlUrl, repository.description, repository.comment])
+    }
+    
+    func delete(repository: Repository) {
+        db.open()
+        defer {
+            db.close()
+        }
+        
+        try? db.executeUpdate("DELETE FROM person WHERE person_id = ?", values: [repository.id])
+    }
+
+    func update(repository: Repository) {
+        db.open()
+        defer {
+            db.close()
+        }
+        
+        try? db.executeUpdate("UPDATE 'person' SET name = ?  WHERE person_id = ? ", values: [repository.name, repository.id])
+        try? db.executeUpdate("UPDATE 'person' SET full_name = ?  WHERE person_id = ? ", values: [repository.fullName, repository.id])
+        try? db.executeUpdate("UPDATE 'person' SET html_url = ?  WHERE person_id = ? ", values: [repository.htmlUrl, repository.id])
+        try? db.executeUpdate("UPDATE 'person' SET description = ?  WHERE person_id = ? ", values: [repository.description,   repository.id])
+        try? db.executeUpdate("UPDATE 'person' SET comment = ?  WHERE person_id = ? ", values: [repository.comment, repository.id])
+    }
+    
+    func getAllRepository() -> [Repository] {
+        db.open()
+        defer {
+            db.close()
+        }
+        
+        var dataArray: [Repository] = []
+        
+        let res: FMResultSet = (try? db.executeQuery("SELECT * FROM person", values: nil)) ?? FMResultSet()
+        
+        while res.next() {
+            let repository = Repository()
+            repository.id = Int(res.int(forColumn: "person_id"))
+            repository.name = res.string(forColumn: "name") ?? ""
+            repository.fullName = res.string(forColumn: "full_name") ?? ""
+            repository.htmlUrl = res.string(forColumn: "html_url") ?? ""
+            repository.description = res.string(forColumn: "description") ?? ""
+            repository.comment = res.string(forColumn: "comment") ?? ""
+            dataArray.append(repository)
+        }
+        
+        return dataArray
+    }
 }
