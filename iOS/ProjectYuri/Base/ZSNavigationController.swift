23fda1bddcbd7251587dc83828fad18fa6340ed2
Zhang Shuai <zhangshuai@bit-ware.co.jp>
2019-03-25 18:20:24 +0900

    Add ZSNavigationController and ZSTabBarController

--- iOS/ProjectYuri/Base/ZSNavigationController.swift
+++ iOS/ProjectYuri/Base/ZSNavigationController.swift
@@ -8,14 +8,53 @@
 
 import UIKit
 
-class ZSNavigationController: UINavigationController {
+class ZSNavigationController: UINavigationController, UIGestureRecognizerDelegate {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /// 自定义返回按钮后返回手势失效
+        /// 手动实现返回手势代理
+        interactivePopGestureRecognizer?.delegate = self
     }
-    */
+    
+    override func pushViewController(_ viewController: UIViewController, animated: Bool) {
+        
+        
+        /// 如果当前控制器的子控制器个数大于等于1, 说明推出的控制器为子控制器
+        if children.count > 0 {
+            
+            /// 自定义返回按钮
+            let backBtn = UIButton()
+            backBtn.setImage(UIImage(named: "IMG_Back"), for: .normal)
+            backBtn.addTarget(self, action: #selector(goBack), for: .touchUpInside)
+            viewController.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backBtn)
+            
+            /// 子控制器隐藏 bottomBar
+            viewController.hidesBottomBarWhenPushed = true
+        }
+        super.pushViewController(viewController, animated: animated)
+    }
+    
+    /// 返回按钮点击事件
+    @objc func goBack() {
+        popViewController(animated: true)
+    }
+    
+    /// 滑动返回手势
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        
+        /// 判断是否需要滑动返回手势
+        if gestureRecognizer == navigationController?.interactivePopGestureRecognizer {
+            return navigationController!.children.count > 1
+        }
+        
+        /// 解决当前控制器为根控制器的时候, 返回手势卡屏的问题
+        if children.count == 1 {
+            return false
+        }
+        
+        return true
+    }
 
 }
--- iOS/ProjectYuri/Base/ZSTabBarController.swift
+++ iOS/ProjectYuri/Base/ZSTabBarController.swift
@@ -15,27 +15,24 @@ class ZSTabBarController: UITabBarController {
         createChildController()
     }
     
-    /// 通过自定义方法添加所有子控制器
     func createChildController() {
-        addChildVC(childVc: HomeViewController(), title: "首页".localized, image: "IMG_Home", selectedImage: "IMG_Home_Selected")
-        addChildVC(childVc: UIViewController(), title: "我的".localized, image: "IMG_More", selectedImage: "IMG_More_Selected")
+        addChildVC(childVc: HomeViewController.self, title: "首页".localized, image: "IMG_Home", selectedImage: "IMG_Home_Selected")
+        addChildVC(childVc: UIViewController.self, title: "我的".localized, image: "IMG_More", selectedImage: "IMG_More_Selected")
     }
     
-    /// 自定义添加子控制器
-    func addChildVC(childVc: UIViewController, title:String, image: String, selectedImage: String) {
-        let nav = ZSNavigationController(rootViewController: childVc)
+    func addChildVC(childVc: UIViewController.Type, title:String, image: String, selectedImage: String) {
+        let nav = ZSNavigationController(rootViewController: childVc.init())
         let normalImage = UIImage(named: image)
         let selectedImage = UIImage(named: selectedImage)
         nav.navigationItem.title = title
-        nav.tabBarItem = tabbarItem(with: title, normalImage: normalImage!, selectedImage: selectedImage!)
+        nav.tabBarItem = tabbarItem(with: title, normalImage: normalImage, selectedImage: selectedImage)
         addChild(nav)
     }
     
-    /// 快捷创建 UITabBarItem
-    func tabbarItem(with title: String, normalImage: UIImage, selectedImage: UIImage) -> UITabBarItem{
-        let image = normalImage.withRenderingMode(.alwaysOriginal)
-        let _selectedImage = selectedImage.withRenderingMode(.alwaysOriginal)
-        let tabBarItem = UITabBarItem(title: title, image: image, selectedImage: _selectedImage)
+    func tabbarItem(with title: String, normalImage: UIImage? = nil, selectedImage: UIImage? = nil) -> UITabBarItem{
+        let normalImage = normalImage?.withRenderingMode(.alwaysOriginal)
+        let selectedImage = selectedImage?.withRenderingMode(.alwaysOriginal)
+        let tabBarItem = UITabBarItem(title: title, image: normalImage, selectedImage: selectedImage)
         tabBarItem.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(hex: MAIN_COLOR)], for: .selected)
         tabBarItem.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor(hex: TABBAR_NORMAL_COLOR)], for: .normal)
         return tabBarItem
