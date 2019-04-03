c5caeade0b532e55843ba184c0323ebe431772d4
Zhang Shuai <macbook-pro.local>
2019-04-03 16:38:23 +0900

    Modify titleSelectedColor, indicatorColor

--- iOS/ProjectYuri/Controller/ProfileContainerViewController.swift
+++ iOS/ProjectYuri/Controller/ProfileContainerViewController.swift
@@ -11,55 +11,63 @@ import SGPagingView
 
 class ProfileContainerViewController: ZSViewController {
     
-    var pageTitleView: SGPageTitleView?
-    var pageContentScrollView: SGPageContentCollectionView?
-
-    override func buildSubViews() {
-        super.buildSubViews()
-        
-        let configure = SGPageTitleViewConfigure()
-        pageTitleView = SGPageTitleView(frame: CGRect.zero, delegate: self, titleNames: ["游戏", "动画", "漫画", "小说"], configure: configure)
-        view.addSubview(pageTitleView!)
-        
-        var vcArray: [AnyHashable] = []
-        for _ in 0..<5 {
+    let titles = ["游戏", "动画", "漫画", "小说"]
+    
+    lazy var vcArray: [AnyHashable] = {
+        var vcArray = [AnyHashable]()
+        for _ in 0..<titles.count {
             let vc = ProfileViewController()
             vc.view.backgroundColor = UIColor.random
             vcArray.append(vc)
         }
-        pageContentScrollView = SGPageContentCollectionView(frame: CGRect.zero, parentVC: self, childVCs: vcArray)
-        pageContentScrollView?.delegatePageContentCollectionView = self
-        view.addSubview(pageContentScrollView!)
+        return vcArray
+    }()
+    
+    lazy var pageTitleView: SGPageTitleView = {
+        let configure = SGPageTitleViewConfigure()
+        configure.indicatorColor = UIColor.main
+        configure.titleSelectedColor = UIColor.main
+        let pageTitleView: SGPageTitleView = SGPageTitleView(frame: CGRect.zero, delegate: self, titleNames:titles , configure: configure)
+        return pageTitleView
+    }()
+    
+    lazy var pageContentScrollView: SGPageContentCollectionView = {
+        let pageContentScrollView: SGPageContentCollectionView = SGPageContentCollectionView(frame: CGRect.zero, parentVC: self, childVCs: vcArray)
+        pageContentScrollView.delegatePageContentCollectionView = self
+        return pageContentScrollView
+    }()
+
+    override func buildSubViews() {
+        super.buildSubViews()
+        view.addSubview(pageTitleView)
+        view.addSubview(pageContentScrollView)
     }
     
     override func makeConstraints() {
         super.makeConstraints()
+        pageTitleView.snp.makeConstraints({ (make) in
+            make.top.equalTo(topLayoutGuide.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(40)
+        })
         
-        if let pageTitleView = pageTitleView, let pageContentScrollView = pageContentScrollView {
-            pageTitleView.snp.makeConstraints({ (make) in
-                make.top.equalTo(topLayoutGuide.snp.bottom)
-                make.leading.trailing.equalToSuperview()
-                make.height.equalTo(40)
-            })
-            
-            pageContentScrollView.snp.makeConstraints({ (make) in
-                make.top.equalTo(pageTitleView.snp.bottom)
-                make.bottom.equalTo(bottomLayoutGuide.snp.top)
-                make.leading.trailing.equalToSuperview()
-                make.height.equalTo(40)
-            })
-        }
+        pageContentScrollView.snp.makeConstraints({ (make) in
+            make.top.equalTo(pageTitleView.snp.bottom)
+            make.bottom.equalTo(bottomLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(40)
+        })
     }
 }
 
 extension ProfileContainerViewController: SGPageTitleViewDelegate, SGPageContentCollectionViewDelegate {
     func pageTitleView(_ pageTitleView: SGPageTitleView, selectedIndex: Int) {
         NSLog("%ld", selectedIndex)
-        pageContentScrollView?.setPageContentCollectionViewCurrentIndex(selectedIndex)
+        pageContentScrollView.setPageContentCollectionViewCurrentIndex(selectedIndex)
     }
     
     func pageContentCollectionView(_ pageContentCollectionView: SGPageContentCollectionView, progress: CGFloat, originalIndex: Int, targetIndex: Int) {
         NSLog("%ld - %ld", originalIndex, targetIndex)
-        pageTitleView?.setPageTitleViewWithProgress(progress, originalIndex: originalIndex, targetIndex: targetIndex)
+        pageTitleView.setPageTitleViewWithProgress(progress, originalIndex: originalIndex, targetIndex: targetIndex)
     }
 }
