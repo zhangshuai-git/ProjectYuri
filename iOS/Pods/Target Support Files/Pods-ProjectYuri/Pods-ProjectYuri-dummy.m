8784dca2fe2c8f4fab6ae5a1fb6e371a7605f465
zhangshuai <zhangshuai@mac.local>
2019-09-12 16:53:32 +0900

    Updated .ignore

--- iOS/Pods/Target Support Files/Pods-ProjectYuri/Pods-ProjectYuri-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ProjectYuri : NSObject
-@end
-@implementation PodsDummy_Pods_ProjectYuri
-@end
--- iOS/Pods/Target Support Files/ZSUtils/ZSUtils-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_ZSUtils : NSObject
-@end
-@implementation PodsDummy_ZSUtils
-@end
